7d1a34236d9a34cf999a9b3495b7b70e7589f78b
Manuel S <masanchezzm@gmail.com>
2021-02-20 13:04:16 +0100

    creado ejemplo de secuencia de promesa-resolve-reject y todo eso

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import {tweetView} from './views.js'
 //Importas servicio que devuelve los tuits
 import dataService from './services/DataService.js'
+import prueba from './prueba.js'
+
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
@@ -11,20 +13,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     //Otra
     loader.classList.add('hidden')
-    //Creamos array de objetos literales para guardar tuits
-    //Luego hemos metido el array en DataService.js/ meth getTweets...
-    //los pillamos de ahí
-    const tweets = dataService.getTweets()
-    const list = document.querySelector('.posts-list')
-    //Teniendo la plantilla html para un tuit, creamos más a partir de ella
-    for(const tweet of tweets){
-        const tweetElement = document.createElement('article');
-        const tweetHTML = tweetView(tweet)
-        //meto el tropezón como código html de const twweet
-        tweetElement.innerHTML=tweetHTML;
-        //Selecciono la etiqueta main mediante su clase .post-list
-        
-        list.appendChild(tweetElement)
+
+    //función que se usa como resolve ( ok) en la promesa
+    const  cargarTweets = (tweets)=>{
+        console.log('BIEN!! VOY A PINTAR LOS TWEETS')
+        const list = document.querySelector('.posts-list')
+        //Teniendo la plantilla html para un tuit, creamos más a partir de ella
+        for(const tweet of tweets){
+            const tweetElement = document.createElement('article');
+            const tweetHTML = tweetView(tweet)
+            //meto el tropezón como código html de const twweet
+            tweetElement.innerHTML=tweetHTML;
+            //Selecciono la etiqueta main mediante su clase .post-list
+            
+            list.appendChild(tweetElement)
+        }
+    }
+    //función que se usa como reject (error) cuando hay un error en la promesa
+    const avisarDelError = (error) => {
+        console.error('NO SE HAN PODIDO CARGAR LOS TWEETS')
     }
 
+    //Creamos array de objetos literales para guardar tuits
+    //Luego hemos metido el array en DataService.js/ meth getTweets...
+    //los pillamos de ahí mediante promesa
+    const tweetsPromise =  dataService.getTweets()
+    //Aquí llega la función "encargo al servidor": si rula, haz usa el callback del then
+    //llamando a resolve...si no rula, usa el callback del catch llamando a reject
+    tweetsPromise.then(cargarTweets).catch(avisarDelError)
+
+ 
 });
--- js/services/DataService.js
+++ js/services/DataService.js
@@ -1,19 +1,35 @@
+function getTheFakinTweets(){
+    return [
+        {
+            author: "@Manuel",
+            message: "Mensaje pruebaaaaa de Manuelllll",
+            date: "19/02/2021 10:30"
+        },
+        {
+            author: "@Paquito",
+            message: "Mensaje pruebaaaaa de Paquitoooo",
+            date: "19/02/2021 23:45"
+        }
+    ]
+
+}
+
+
 export default{
     //Servicio que devuelve los tuits
     getTweets: ()=>{
-        return [
-            {
-                author: "@Manuel",
-                message: "Mensaje pruebaaaaa de Manuelllll",
-                date: "19/02/2021 10:30"
-            },
-            {
-                author: "@Paquito",
-                message: "Mensaje pruebaaaaa de Paquitoooo",
-                date: "19/02/2021 23:45"
-            }
-    
-    
-        ]
+        const promise = new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                console.log('HE CONSEGUIDO LOS TWEETS!!!!!')
+                const tweets = getTheFakinTweets()
+                if( tweets.length===0){
+                    reject(tweets)
+                }else{
+                    resolve(tweets)
+                }
+            }, 5000);
+        })
+        console.log('TE PROMETO QUE CONSEGUIRÉ LOS TUITS, NOMÁS DEJA QUE GESTIONE LA PROMESA')
+        return promise
     }
 }
