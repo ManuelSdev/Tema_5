4ea79165c55ac95d2192006b4c4ad6a69e7fc9b6
Manuel S <masanchezzm@gmail.com>
2021-02-22 16:16:10 +0100

    Añadido cierre de panel de error clicando X y fuera del panel

--- js/controllers/ErrorController.js
+++ js/controllers/ErrorController.js
@@ -6,7 +6,14 @@ export default class ErrorController extends BaseControler{
     showError(errorMessage){
         this.element.innerHTML = errorView(errorMessage)
         this.element.classList.remove('hidden')
-
+        //Este evento esconde el panel error al clicar fuera de él 
+        //Fuera de él es .global-errors
+        //El manejador recibe como parámetro un objeto de tipo evento (event)
+        this.element.addEventListener('click', (event)=>{  
+            if(event.target== this.element || event.target.classList.contains('delete')){
+                this.element.classList.add('hidden')
+            }
+        })
     }
 
 }
